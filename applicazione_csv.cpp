8bb9474e48bf1a46f5609741fd99841a1e61a22d
mazzoleni <mazzoleni.diego.studente@itispaleocapa.it>
2026-05-08 10:42:02 +0200

    Add files via upload

---
+++ applicazione_csv.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct location
+	{
+		double lon;
+		double lat;
+	};
+	
+	struct dati
+	{
+		string ClasseToponimo;
+		string DescrizioneToponimo;
+		string Numero;
+		string Subalterno;
+		string CAP;
+		string SezioneISTAT;
+		double Lat;
+		double Lon;
+		location Posto;
+	};  
+	
+
+	
+int main()
+{
+	
+ 
+
+    return 0;
+}
