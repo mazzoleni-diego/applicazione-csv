50ae2bda05aeef4cffc9eaf029d2a5229e232645
mazzoleni <mazzoleni.diego.studente@itispaleocapa.it>
2026-05-08 11:59:29 +0200

    Add files via upload

--- applicazione_csv.cpp
+++ applicazione_csv.cpp
@@ -8,8 +8,8 @@ struct location
 		double lon;
 		double lat;
 	};
-	
-	struct dati
+
+	struct NumerazioneCivica
 	{
 		string ClasseToponimo;
 		string DescrizioneToponimo;
@@ -21,12 +21,54 @@ struct location
 		double Lon;
 		location Posto;
 	};  
-	
 
-	
+void visualizza_struttura(NumerazioneCivica dati[]) 
+{
+
+}
+
+void lettura_struttura(NumerazioneCivica dati[])
+{
+	    ifstream fileInput("Comune_Bergamo_-_Numerazione_civica.csv"); 
+    string linea;
+
+    if (fileInput.is_open()) 
+	{
+        while (fileInput >> linea)
+		{ 
+        	cout << linea << endl;
+        	i++;
+        }
+        fileInput.close();
+    } else 
+	{
+        cout << "Impossibile aprire il file";
+    }
+}
+
 int main()
 {
-	
+	int cont = 3;
+	NumerazioneCivica dati[1000];	
+
+	do
+	{	
+		cout << "\n0 - Esci"<<endl;
+		cout << "1 - Lettura"<<endl;
+		cout << "2 - Visualizzazione"<<endl;
+		cout<< "scegli la funzione:";
+		cin >> cont;
+		
+		switch (cont)
+		{
+			case 1:
+				lettura_struttura(dati);
+				break;
+			case 2:
+				visualizza_struttura( dati);
+				break;				
+		}
+	}while(cont != 0);
  
 
     return 0;
